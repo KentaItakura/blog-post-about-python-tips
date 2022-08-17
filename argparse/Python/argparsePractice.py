1f883621f4ddaad9bbf46c7fb87e47ea67370854
KentaItakura <kentai@scanx.jp>
2022-08-17 20:52:15 +0900

    [Update] コマンドの例を記載

--- argparse/Python/argparsePractice.py
+++ argparse/Python/argparsePractice.py
@@ -1,13 +1,23 @@
+# コマンドの例
+# python argparsePractice.py 10 --input_path D:\blog -a abbreviation
+
+# helpを確認する
+# python argparsePractice.py --help or python argparsePractice.py -h
+
+# import module
+import argparse
+
+# argparseの中身の定義
 parser = argparse.ArgumentParser()
-parser.add_argument('arg1', type=float')
+parser.add_argument('firstArg1', type=float) # float, stringなどの変数の型を指定できる
 parser.add_argument('--input_path', type=str, default='None', help='input path')
-parser.add_argument('--label_path', type=str, default='None', help='label path')
-parser.add_argument('--output_path', type=str, default='None', help='output path')
-# parser.add_argument('--model_path', type=str, default='./results/Log_2021-02-28_16-17-18', help='pretrained model path')
+parser.add_argument('--value1', type=float, default=0.5, help='label path')
+parser.add_argument('-a','--long_name', type=str, default='None', help='abbreviation')
+# argsに、引数の値が格納される
 args = parser.parse_args()
 
-# Specify input path
-print('arg1='+args.arg1)
-print('arg2='+args.input_path)
-print('arg3='+args.arg3)
-print('arg4='+args.arg4)
+# パラメーターの確認
+print('firstArg1 = '+str(args.firstArg1))
+print('input_path = '+args.input_path)
+print('value1 = '+str(args.value1))
+print('long_name = '+args.long_name) # 省略後ではなく、正式名で参照する
